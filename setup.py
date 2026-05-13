0936af4d5c5103e297cc58baddd60b2ccc555bc6
subratahowlader <83128067+subratahowlader@users.noreply.github.com>
2026-05-14 03:42:56 +0600

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Subrata",
+    author_email="contact@mysubrata.com",
+    packages=find_packages(),
+    install_requires=[],
+)
